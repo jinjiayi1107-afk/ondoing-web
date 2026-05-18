500c28785e8b0d3e89977d74f51112e38a0a0335
jinjiayi1107-afk <jinjiayi1107@gmail.com>
2026-05-18 09:31:01 +0800

    Fix Supabase project URL

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const fallbackUrl = 'https://ugpubhranolyykjdltqy.supabase.co'
+const fallbackUrl = 'https://ugpubhranolyvkjdltqy.supabase.co'
 const fallbackAnonKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVncHViaHJhbm9seXZramRsdHF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MTE2OTUsImV4cCI6MjA5NDM4NzY5NX0.O7Ghc16f0hfTrL4JkPdAH1JZa4BeOoSywxyQXiVdT2Q'
 
